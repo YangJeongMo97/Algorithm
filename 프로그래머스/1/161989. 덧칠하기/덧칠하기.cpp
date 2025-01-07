c89d706efe5234128a2aa339290ac79e92702b8b
GoBo_GameDev <74939051+YangJeongMo97@users.noreply.github.com>
2025-01-07 10:11:50 +0900

    [level 1] Title: 덧칠하기, Time: 0.30 ms, Memory: 5.52 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int end = 0;
+    
+    for(int i = 0; i < section.size(); i++)
+    {
+        if(section[i] > end)
+        {
+            end = section[i] + m - 1;
+            answer++;
+        }
+    }
+    
+    return answer;
+}
