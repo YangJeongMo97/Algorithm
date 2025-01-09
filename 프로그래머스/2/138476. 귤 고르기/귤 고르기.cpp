e73ed4a0db1821fcedc0b38411767175c5c808bf
GoBo_GameDev <74939051+YangJeongMo97@users.noreply.github.com>
2025-01-09 10:46:38 +0900

    [level 2] Title: 귤 고르기, Time: 1579.54 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(pair<int, int>& a, pair<int, int>& b)
+{
+    if(a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+}
+
+int solution(int k, vector<int> tangerine) {
+    int answer = 0;
+    int size = tangerine.size();
+    unordered_map<int, int> um;
+    
+    for(int i = 0; i < tangerine.size(); i++)
+    {
+        um[tangerine[i]]++;
+    }
+    
+    vector<pair<int, int>> vp(um.begin(), um.end());
+    
+    sort(vp.begin(), vp.end(), cmp);
+    
+    answer = vp.size();
+    
+    for(int i = 0; i < size - k; i++)
+    {
+        if(vp[0].second > 1) vp[0].second--;
+        else
+        {
+            vp.erase(vp.begin());
+            answer--;
+        }
+    }
+    
+    return answer;
+}
